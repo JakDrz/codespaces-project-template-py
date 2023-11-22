e60d38a912bd70032891436d2c3770e8d0447e9a
JakDrz <147598433+JakDrz@users.noreply.github.com>
2023-11-22 09:58:40 +0000

    06.12

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -2,7 +2,7 @@ from time import sleep
 #from snap7.server import mainloop
 import requests
 
-res = requests.get("https://api.openaq.org/v2/locations/2178")
+res = requests.get("https://api.openaq.org/v2/locations/2178", , headers={"X-API-Key": "c07e50cfebdade8eb5e2dc0a5e29ea2e8a601d7ad9f5eb0ddfc8255f7609c88a"})
 
 #https://docs.openaq.org/docs/getting-started
 
